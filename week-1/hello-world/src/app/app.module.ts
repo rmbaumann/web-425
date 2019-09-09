c1feb14613c9922abcc5af2710eacf767677da98
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-09-09 18:52:14 -0500

    Update app.module.ts

--- week-1/hello-world/src/app/app.module.ts
+++ week-1/hello-world/src/app/app.module.ts
@@ -1,16 +1,26 @@
+/*
+============================================
+; Title: Exercise 1.3
+; Author: Reva Baumann
+; Date: 09 Sept 2019
+; Modified By: Reva Baumann
+; Description: Angular CLI
+;===========================================
+*/
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

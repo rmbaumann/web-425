68e7c894412f93751da6fd349113102ae02423d8
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-09-10 17:06:33 -0500

    Create shipping.component.ts

---
+++ week-1/hello-modules/src/app/shipping/shipping.component.ts
@@ -0,0 +1,32 @@
+/*
+============================================
+; Title: Assignment 1.5
+; Author: Reva Baumann
+; Date: 09 Sept 2019
+; Modified By: Reva Baumann
+; Description: Hello Modules
+;===========================================
+*/
+
+// Start Program
+
+// Declare details of component, define
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping',
+  template: `
+    <p>Welcome to the Shipping Component and Specs!</p>
+  `,
+  styles: []
+})
+
+// Export the component class
+export class ShippingComponent implements OnInit {
+  constructor() { }
+  ngOnInit() {
+  }
+
+}
+
+// end program

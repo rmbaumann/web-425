400559f26618708d2f8c7a6a7f7a815a48180046
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-09-09 19:13:26 -0500

    Create baumann-assignment-1.4.js

---
+++ week-1/es5-typescript-example/baumann-assignment-1.4.js
@@ -0,0 +1,23 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Reva Baumann
+; Date: 09 Sept 2019
+; Modified By: Reva Baumann
+; Description: Typescript Example
+;===========================================
+*/
+/* Expected Output:
+Hello Reva Baumann
+*/
+// Start Program
+// Create a string of values and assign
+var firstName = 'Reva';
+var lastName = 'Baumann';
+// Declare functiton and return string details
+function myName(firstName, lastName) {
+    return 'Hello ' + firstName + ' ' + lastName;
+}
+// Output
+console.log(myName(firstName, lastName));
+// End Program

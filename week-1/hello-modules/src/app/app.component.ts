3253e54e4f4544c37f84e35a71028a43c18db707
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-09-10 17:26:39 -0500

    Update app.component.ts

--- week-1/hello-modules/src/app/app.component.ts
+++ week-1/hello-modules/src/app/app.component.ts
@@ -1,3 +1,15 @@
+/*
+============================================
+; Title: Assignment 1.5
+; Author: Reva Baumann
+; Date: 09 Sept 2019
+; Modified By: Reva Baumann
+; Description: Hello Modules
+;===========================================
+*/
+
+// Start Program
+
 import { Component } from '@angular/core';
 
 @Component({

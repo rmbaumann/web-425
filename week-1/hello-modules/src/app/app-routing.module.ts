1b9cfff2014463efa2894b72db3b53ed70e61f69
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-09-11 18:21:47 -0500

    Update app-routing.module.ts

--- week-1/hello-modules/src/app/app-routing.module.ts
+++ week-1/hello-modules/src/app/app-routing.module.ts
@@ -1,3 +1,15 @@
+/*
+============================================
+; Title: Assignment 1.5
+; Author: Reva Baumann
+; Date: 09 Sept 2019
+; Modified By: Reva Baumann
+; Description: Hello Modules
+;===========================================
+*/
+
+// Start Program
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 

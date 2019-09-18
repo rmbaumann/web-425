57fdfc1e1e8b87640521b7dd504c01b2ac42287a
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-09-18 18:53:24 -0500

    Update app.component.ts

--- week-2/two-way-binding/src/app/app.component.ts
+++ week-2/two-way-binding/src/app/app.component.ts
@@ -1,29 +1,25 @@
-/*
-============================================
+/*============================================
 ; Title: app.component.ts
 ; Author: Reva Baumann
 ; Date: 16 September 2019
-; Modified By: Reva Baumann
-; Description: Two-Way Binding
+; Description:  two-way-binding
 ;===========================================
 */
 
-// Start Program
+// start program
 
-// Import the Angular Module
+// Import Module
 import { Component } from '@angular/core';
 
-// Declare the component
+//Component with inline html/style
 @Component({
   selector: 'app-root',
 
-  // Template
   template: `
-  <br /><br /><br />
-
+  <!--  Navigation Bar and Details -->
   <header>
-      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="#">Two Way Binding Example</a>
+      <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
+        <a class="navbar-brand" href="#">Two Way Binding</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -33,7 +29,7 @@ import { Component } from '@angular/core';
               <a class="nav-link" href="#">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">View</a>
+              <a class="nav-link" href="#">List</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">New</a>
@@ -42,40 +38,38 @@ import { Component } from '@angular/core';
         </div>
       </nav>
     </header>
+    <div class="container-fluid two-way-form w-50">
+      <h2>Two-Way Binding Example</h2><br />
+      <div class="form-group">
+        <input class="form-control" type="text"
 
-  <div class="container-fluid two-way-form">
-  <h2>Two-Way Binding Example</h2><br />
-  <div class="form-group">
-      <input class="form-control" type="text"
-            placeholder="Enter your Name..."
-            [(ngModel)]="name" #ctrl="ngModel">
-
-      <button class="btn btn-primary form-control" (click)="name=''">Clear Name</button>
+                placeholder="Enter your name ..."
+                [(ngModel)]="name" #ctrl="ngModel">
+        
+        <button class="btn btn-primary form-control" (click)="name=''">Clear Name</button>
+      </div>
+      <div class="form-group">
+        <p class="alert-sucess">Welcome to two-way binding module {{name}} </p>
+      </div>
     </div>
+  `,
 
-    <div class="form-group">
-        <p class="alert-success">Welcome to two-way binding, {(name)}! </p>
-    </div>
-
-  </div>
-
-`,
-
-styles: [`
-  h1{
+  // Styles
+  styles: [`
+  p{
     text-align: center;
   }
-  p{
-    margin: 10px;
-    text-align: center;
-
+  .form-control{
+    background:blue;
   }`]
+    
 })
 
-// Export component
+// Export the component
 export class AppComponent {
   title = 'two-way-binding';
   name:string = 'Reva Baumann'
-} 
 
-// End Program
+}
+
+// end program

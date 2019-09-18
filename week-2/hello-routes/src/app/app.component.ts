4136c4f7225162ec9a42b49f2bcc5b26f3441951
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-09-17 19:45:58 -0500

    Create app.component.ts

---
+++ week-2/hello-routes/src/app/app.component.ts
@@ -0,0 +1,22 @@
+/*
+============================================
+; Title: app.component.ts
+; Author: Reva Baumann
+; Date: 09/14/2019
+; Modified By: Reva Baumann
+; Description: Routing in Action
+;===========================================
+*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'hello-routes';
+}
+
+// end program

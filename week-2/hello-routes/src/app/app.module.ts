fe3382f34fa1c1e5b346b804f2dee5e7054ac869
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-09-17 19:46:00 -0500

    Create app.module.ts

---
+++ week-2/hello-routes/src/app/app.module.ts
@@ -0,0 +1,36 @@
+/*
+============================================
+; Title: app.module.ts
+; Author: Reva Baumann
+; Date: 09/14/2019
+; Modified By: Reva Baumann
+; Description: Routing in Action
+;===========================================
+*/
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+// end program

c42c1e7fed33b8004a41bb2ff492309c1f46b589
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-09-25 18:47:09 -0500

    Update app.module.ts

--- week-3/guards/src/app/app.module.ts
+++ week-3/guards/src/app/app.module.ts
@@ -1,3 +1,16 @@
+/*
+============================================
+; Title: app.module.ts
+; Author: Reva Baumann
+; Date: 09/24/2019
+; Modified By: Reva Baumann
+; Description: Guarding Routes
+;===========================================
+*/
+
+// Start program
+
+// Import the Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -6,6 +19,10 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { LoginGuard } from './login.guard';
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
+import { ReactiveFormsModule } from '@angular/forms';
+import { UnsavedChangesGuard } from './unsaved-changes.guard';
 
 @NgModule({
   declarations: [
@@ -16,9 +33,10 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [ { provide: LocationStrategy, useClass: HashLocationStrategy}, LoginGuard, UnsavedChangesGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

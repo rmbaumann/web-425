011d450c075607960e18aee2edff93250f15caf1
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-09-25 17:49:40 -0500

    Create app.module.ts

---
+++ week-3/child-routes/src/app/app.module.ts
@@ -0,0 +1,43 @@
+/*
+============================================
+; Title: app.module.ts
+; Author: Reva Baumann
+; Date: 23 September 2019
+; Modified By: Reva Baumann
+; Description: app.modulee.ts
+;===========================================
+*/
+
+// start program
+
+// Import the modules
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
+import { SellerInfoComponent } from './seller-info/seller-info.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductDetailComponent,
+    ProductDescriptionComponent,
+    SellerInfoComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
+
+
+// end program

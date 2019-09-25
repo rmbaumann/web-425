c08c5a72c8540a3f520d61667603220344a4fa65
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-09-25 18:47:07 -0500

    Update product.component.ts

--- week-3/guards/src/app/product/product.component.ts
+++ week-3/guards/src/app/product/product.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+/*
+============================================
+; Title: product.component.ts
+; Author: Reva Baumann
+; Date: 09/24/2019
+; Modified By: Reva Baumann
+; Description: Guarding Routes
+;===========================================
+*/
 
+// start program
+
+// Import the MOdules
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+// Component
 @Component({
   selector: 'app-product',
   template: `
-    <p>
-      product works!
-    </p>
+    <h1>Welcome to the Product Component!</h1>
+    <input placeholder="What's your favorite color?" type=text [formControl]="name">
   `,
-  styles: []
+  styles: [`
+    h1 {
+      color: red;
+    }
+  `]
 })
+
+// Export the class and execute
 export class ProductComponent implements OnInit {
+
+  name: FormControl = new FormControl();
 
   constructor() { }
 
@@ -17,3 +39,4 @@ export class ProductComponent implements OnInit {
   }
 
 }
+// end program

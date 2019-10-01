4841960f134d30c347c72acaefb8a79fe6ba91f4
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-09-30 19:59:51 -0500

    Create fruit.ts

---
+++ week-4/hello-di/src/app/fruit/fruit.ts
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title: fruit.ts
+; Author: Reva Baumann
+; Date: 29 September 2019
+; Modified By: Reva Baumann
+; Description: Inversion of Control & Dependency Injection
+;===========================================
+*/
+
+// Export the class to list details
+export class Fruit {
+    constructor(public id: number, public name: string, public color: string) {}
+}

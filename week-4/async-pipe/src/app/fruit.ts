aee17ef6f6c67bdec5cb275bcd7994f675e1f199
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-10-02 18:36:01 -0500

    Create fruit.ts

---
+++ week-4/async-pipe/src/app/fruit.ts
@@ -0,0 +1,20 @@
+/*============================================
+; Title: fruit.ts
+; Author: Reva Baumann
+; Date: 2 October 2019
+; Description: Async Pipe
+;===========================================
+*/
+
+// Start Program
+
+// Export the details of the fruits
+export class Fruit {
+    id: number;
+    name: string;
+    pricePerPound: string;
+    quantity: number;
+}
+// end program
+
+// Start Program

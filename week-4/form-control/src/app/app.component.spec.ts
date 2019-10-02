1813dc8266fa2d2baa4ef7f33fe42eae89b3067a
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-10-02 17:28:31 -0500

    Create app.component.spec.ts

---
+++ week-4/form-control/src/app/app.component.spec.ts
@@ -0,0 +1,43 @@
+/*============================================
+; Title: app.component.spec.ts
+; Author: Reva Baumann
+; Date: 2 October 2019
+; Description:  Event Handling
+;===========================================
+*/
+
+
+// start program
+
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'donner-form-control'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('donner-form-control');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('donner-form-control app is running!');
+  });
+});
+// end program

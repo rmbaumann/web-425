3279a9ecbc8deceb4e4674a19d50712eaf32a991
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-10-09 17:32:48 -0500

    Create app.component.ts

---
+++ week-5/baumann-md-in-action/src/app/app.component.ts
@@ -0,0 +1,28 @@
+/*
+============================================
+; Title: Exercise 5.4
+; Author: Reva Baumann
+; Date: 09 October 2019
+; Modified By: Reva Baumann
+; Description: Flex-Layout
+;===========================================
+*/
+
+// Start Program
+
+// Import the Angular Module
+import { Component } from '@angular/core';
+
+// Declare the component
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+// Export the class
+export class AppComponent {
+  title = 'baumann-md-in-action';
+}
+
+// End Program

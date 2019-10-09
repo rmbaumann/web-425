908b869541bcbb316f31477e8574b5a7755758e9
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-10-09 16:19:59 -0500

    Create app.component.ts

---
+++ week-5/baumann-md-toolbar/src/app/app.component.ts
@@ -0,0 +1,29 @@
+
+/*
+============================================
+; Title: Exercise 5.2
+; Author: Reva Baumann
+; Date: 09 October 2019
+; Modified By: Reva Baumann
+; Description: Navigation
+;===========================================
+*/
+
+// Start Program
+
+// Import the module
+import { Component } from '@angular/core';
+
+// Declare the Component
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+// Export the class
+export class AppComponent {
+  title = 'baumann-md-toolbar';
+}
+
+// End Program

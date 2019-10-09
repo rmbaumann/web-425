7d46269f7d586b0333809d91eee4a01484949ff8
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-10-09 17:32:15 -0500

    Create home.component.spec.ts

---
+++ week-5/baumann-md-in-action/src/app/home/home.component.spec.ts
@@ -0,0 +1,35 @@
+/*
+============================================
+; Title: Exercise 5.4
+; Author: Reva Baumann
+; Date: 09 October 2019
+; Modified By: Reva Baumann
+; Description: Flex-Layout
+;===========================================
+*/
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeComponent } from './home.component';
+
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HomeComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

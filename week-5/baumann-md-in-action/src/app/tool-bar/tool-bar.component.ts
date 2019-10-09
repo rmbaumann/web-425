deeaaf259a15611fa43a7948856b08441d3298af
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-10-09 17:31:56 -0500

    Create tool-bar.component.ts

---
+++ week-5/baumann-md-in-action/src/app/tool-bar/tool-bar.component.ts
@@ -0,0 +1,28 @@
+/*
+============================================
+; Title: Exercise 5.4
+; Author: Reva Baumann
+; Date: 09 October 2019
+; Modified By: Reva Baumann
+; Description: Flex-Layout
+;===========================================
+*/
+
+// Start Program
+
+// Import the module
+import { Component, OnInit } from '@angular/core';
+
+// Declare the component
+@Component({
+    selector: 'app-tool-bar',
+    templateUrl: './tool-bar.component.html',
+    styleUrls: ['./tool-bar.component.css']
+})
+
+// Export the Class
+export class ToolBarComponent implements OnInit {
+    ngOnInit() {}
+}
+
+// End Program

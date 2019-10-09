4291d9a329720d3cf0698f86ed231f5a71e1e91b
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-10-09 17:32:04 -0500

    Create login.component.ts

---
+++ week-5/baumann-md-in-action/src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+/*
+============================================
+; Title: Exercise 5.4
+; Author: Reva Baumann
+; Date: 09 October 2019
+; Modified By: Reva Baumann
+; Description: Flex-Layout
+;===========================================
+*/
+
+// Start Program
+
+// Import the Modules
+import { Component, OnInit } from '@angular/core';
+
+// Declare the Component
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+})
+
+// Declare and export the class
+export class LoginComponent implements OnInit {
+    ngOnInit() {}
+}
+
+// End Program

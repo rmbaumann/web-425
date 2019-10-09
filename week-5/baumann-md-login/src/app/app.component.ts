fbba69e22db41f0b147aae96b2601970af1ab342
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-10-09 16:36:33 -0500

    Create app.component.ts

---
+++ week-5/baumann-md-login/src/app/app.component.ts
@@ -0,0 +1,29 @@
+
+/*
+============================================
+; Title: Exercise 5.3
+; Author: Reva Baumann
+; Date: 09 October 2019
+; Modified By: Reva Baumann
+; Description: Material Cards
+;===========================================
+*/
+
+// Start Program
+
+// Import the Modules
+import { Component } from '@angular/core';
+
+// Declare the comoponent
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+// Export the class
+export class AppComponent {
+  title = 'baumann-md-login';
+}
+
+// End Program

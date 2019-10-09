d65d8baeaeaaf812f45cf6862c3f1cf812eefe71
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-10-09 17:32:12 -0500

    Create home.component.ts

---
+++ week-5/baumann-md-in-action/src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+/*
+============================================
+; Title: Exercise 5.4
+; Author: Reva Baumann
+; Date: 09 October 2019
+; Modified By: Reva Baumann
+; Description: Flex-Layout
+;===========================================
+*/
+
+// Start Program
+
+// Import the Modules
+import { Component, OnInit } from '@angular/core';
+
+// Declare the component
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
+})
+
+// Export the class
+export class HomeComponent implements OnInit {
+    ngOnInit() {}
+}
+
+// End Program

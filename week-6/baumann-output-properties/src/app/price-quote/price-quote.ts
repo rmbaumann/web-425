53cda1b32a22211adf61109435d0caaf3b53e530
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-10-15 22:36:26 -0500

    Create price-quote.ts

---
+++ week-6/baumann-output-properties/src/app/price-quote/price-quote.ts
@@ -0,0 +1,19 @@
+/*
+============================================
+; Title: Exercise 6.3
+; Author: Reva Baumann
+; Date: 14 October 2019
+; Modified By: Reva Baumann
+; Description: Output Properties
+;===========================================
+*/
+
+// Start Program
+
+// Declare and export the interface
+export interface PriceQuote {
+    stockSymbol: string;
+    lastPrice: number;
+}
+
+// End Program

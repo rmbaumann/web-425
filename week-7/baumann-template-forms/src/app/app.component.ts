cdcf91bf69aa686a14ec87a91e7eecb47d67a626
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-10-22 20:17:29 -0500

    Create app.component.ts

---
+++ week-7/baumann-template-forms/src/app/app.component.ts
@@ -0,0 +1,30 @@
+/*
+============================================
+; Title: Exercise 7.2
+; Author: Reva Baumann
+; Date: 19 October 2019
+; Modified By: Reva Baumann
+; Description: Template-Driven Forms
+;===========================================
+*/
+
+// Start Program
+
+// Import the module
+import { Component } from '@angular/core';
+
+// Declare the compoonent
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+// Export the component class
+export class AppComponent {
+  title = 'baumann-template-forms';
+
+  }
+
+
+// End Program

59e87c525e36a742a864e685007ca6a4a9cb2d3a
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-10-22 20:38:15 -0500

    Create app.component.ts

---
+++ week-7/baumann-form-validation/src/app/app.component.ts
@@ -0,0 +1,28 @@
+/*
+============================================
+; Title: Exercise 7.2
+; Author: Reva Baumann
+; Date: 19 October 2019
+; Modified By: Reva Baumann
+; Description: Form Validation
+;===========================================
+*/
+
+// Start Program
+
+// Import the Modules
+import { Component } from '@angular/core';
+
+// Declare the component
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+// Declare the export
+export class AppComponent {
+  title = 'baumann-form-validation';
+}
+
+// End Program

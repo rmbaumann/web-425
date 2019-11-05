975a5428f327c7971edb442dba6545f394115477
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-11-04 18:35:36 -0600

    Create nav-bar.component.ts

---
+++ baumann-bobs/bobs-computer-repair-shop/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,31 @@
+/*
+============================================
+; Title: Bob's Computer Repair Shop
+; Author: Reva Baumann
+; Date: 03 November, 2019
+; Modified By: Reva Baumann
+; Description: Web 425 Week 9 Project
+;===========================================
+*/
+
+// Start Program
+
+// import the modules
+import { Component, OnInit } from '@angular/core';
+
+// component details
+@Component({
+    selector: 'app-nav-bar',
+    templateUrl: './nav-bar.component.html',
+    styleUrls: ['./nav-bar.component.css']
+})
+
+// Export the component
+export class NavBarComponent implements OnInit {
+    constructor () {}
+    ngOnInit() {
+
+    }
+}
+ 
+// End Program

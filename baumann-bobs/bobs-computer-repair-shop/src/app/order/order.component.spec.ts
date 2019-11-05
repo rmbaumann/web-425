f1ba9bf03007ca466231d8271e19da82676481ac
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-11-04 18:35:27 -0600

    Create order.component.spec.ts

---
+++ baumann-bobs/bobs-computer-repair-shop/src/app/order/order.component.spec.ts
@@ -0,0 +1,40 @@
+/*
+============================================
+; Title: Bob's Computer Repair Shop
+; Author: Reva Baumann
+; Date: 03 November, 2019
+; Modified By: Reva Baumann
+; Description: Web 425 Week 9 Project
+;===========================================
+*/
+
+// Start Program
+
+// Import modules
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { OrderComponent } from './order.component';
+
+// Describe the component deetails
+describe('OrderComponent', () => {
+  let component: OrderComponent;
+  let fixture: ComponentFixture<OrderComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ OrderComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OrderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
+// End Program

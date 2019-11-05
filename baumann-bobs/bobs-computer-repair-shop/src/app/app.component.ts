257ad8c11845f11b2ddecc10309ca3d8ad079f14
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-11-04 18:36:02 -0600

    Create app.component.ts

---
+++ baumann-bobs/bobs-computer-repair-shop/src/app/app.component.ts
@@ -0,0 +1,26 @@
+/*
+============================================
+; Title: Bob's Computer Repair Shop
+; Author: Reva Baumann
+; Date: 03 November, 2019
+; Modified By: Reva Baumann
+; Description: Web 425 Week 9 Project
+;===========================================
+*/
+
+// Start Program
+
+// Import the module
+import { Component } from '@angular/core';
+
+// Component Details
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'bobs-computer-repair-shop';
+}
+
+// End Program

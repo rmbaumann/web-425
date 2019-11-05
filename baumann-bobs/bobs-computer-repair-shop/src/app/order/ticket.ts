7483c66d147c355dcf7fbb96576e6c30cabd09c7
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-11-04 18:35:22 -0600

    Create ticket.ts

---
+++ baumann-bobs/bobs-computer-repair-shop/src/app/order/ticket.ts
@@ -0,0 +1,27 @@
+/*
+============================================
+; Title: Bob's Computer Repair Shop
+; Author: Reva Baumann
+; Date: 03 November, 2019
+; Modified By: Reva Baumann
+; Description: Web 425 Week 9 Project
+;===========================================
+*/
+
+// Start Program
+
+// Import the modules
+import { NumberFormatStyle, CurrencyPipe } from '@angular/common';
+
+// export the ticket to calculate
+export class ticket {
+    service: string;
+    price: number;
+}
+
+export class invoiceTotal {
+    sum: string;
+    invoiceTotal: number;
+}
+
+// End Program

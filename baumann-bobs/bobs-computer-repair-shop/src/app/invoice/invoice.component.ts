6be8bc2ee7bb33d4a41db3d3142f862bb88f8109
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-11-04 18:35:49 -0600

    Create invoice.component.ts

---
+++ baumann-bobs/bobs-computer-repair-shop/src/app/invoice/invoice.component.ts
@@ -0,0 +1,40 @@
+/*
+============================================
+; Title: Bob's Computer Repair Shop
+; Author: Reva Baumann
+; Date: 03 November, 2019
+; Modified By: Reva Baumann
+; Description: Web 425 Week 9 Project
+;===========================================
+*/
+
+// Start Program
+
+// Import the modules
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+
+
+import { Component, OnInit, Input, Inject } from '@angular/core';
+
+// Component Deetails
+@Component({
+    selector: 'app-invoice',
+    templateUrl: './invoice.component.html',
+    styleUrls: ['./invoice.component.css']
+  })
+  export class InvoiceComponent implements OnInit {
+    services: [];
+    totalOrder: [];
+    today: number = Date.now();
+    
+  
+    constructor(private dialogRef: MatDialogRef<InvoiceComponent>, @Inject(MAT_DIALOG_DATA) data) {
+        this.services = data.ticket;
+        this.totalOrder = data.invoiceTotal;
+    }
+  
+    ngOnInit() {
+    }
+  }
+
+  // End Program

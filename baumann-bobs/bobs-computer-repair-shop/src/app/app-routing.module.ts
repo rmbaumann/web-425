67bb016716be0f8395081152e6989628cf32cd83
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-11-05 21:04:24 -0600

    Update app-routing.module.ts

--- baumann-bobs/bobs-computer-repair-shop/src/app/app-routing.module.ts
+++ baumann-bobs/bobs-computer-repair-shop/src/app/app-routing.module.ts
@@ -15,16 +15,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OrderComponent } from './order/order.component';
 
-// Routing Details
 const routes: Routes = [
-  { path: ' ', component: OrderComponent }
+  { path: '', component: OrderComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-
 export class AppRoutingModule { }
-
-// End Program

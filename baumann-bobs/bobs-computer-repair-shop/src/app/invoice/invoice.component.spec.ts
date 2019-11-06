a38d4778cb526903f1dc9162156b3d31754245ed
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-11-05 21:04:29 -0600

    Update invoice.component.spec.ts

--- baumann-bobs/bobs-computer-repair-shop/src/app/invoice/invoice.component.spec.ts
+++ baumann-bobs/bobs-computer-repair-shop/src/app/invoice/invoice.component.spec.ts
@@ -9,30 +9,28 @@
 */
 
 // Start Program
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { InvoiceComponent } from './invoice.component';
+import { Component, OnInit, Input, Inject } from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 
-// describe the details of the testing modulee and invoicing
-describe('InvoiceComponent', () => {
-    let component: InvoiceComponent;
-    let fixture: ComponentFixture<InvoiceComponent>;
+@Component({
+  selector: 'app-invoice',
+  templateUrl: './invoice.component.html',
+  styleUrls: ['./invoice.component.css']
+})
+export class InvoiceComponent implements OnInit {
+  services: [];
+  total: [];
+  today: number = Date.now();
   
-    beforeEach(async(() => {
-      TestBed.configureTestingModule({
-        declarations: [ InvoiceComponent ]
-      })
-      .compileComponents();
-    }));
-  
-    beforeEach(() => {
-      fixture = TestBed.createComponent(InvoiceComponent);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    });
-  
-    it('should create', () => {
-      expect(component).toBeTruthy();
-    });
-  });
+
+  constructor(private dialogRef: MatDialogRef<InvoiceComponent>, @Inject(MAT_DIALOG_DATA) data) {
+      this.services = data.ticket;
+      this.total = data.total;
+  }
+
+  ngOnInit() {
+  }
+
+}
 
   // End Program 
